600323c21f5cd2fc63f5b05331200dfc9dbab4cd
Leo Freitas <leofreitas.engineer@gmail.com>
2023-11-05 12:42:57 -0300

    feat: add docker compose file and configured the gateway api and postgres data base

--- projects/gateway-pagamento-api/index.js
+++ projects/gateway-pagamento-api/index.js
@@ -4,7 +4,6 @@ const app = express()
 const port = 3000
 
 const jsonParser = bodyParser.json()
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', (req, resp) => {
   resp.send({
